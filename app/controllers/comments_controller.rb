66f8e62b15d4478bc121415e1b1a86af963439c0
Kiran Ryali <kiran.ryali@gmail.com>
2010-12-23 02:08:14 -0500

    defined controller and view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,7 @@
 class CommentsController < ApplicationController
 	def create
-		@post = Post.find(params[:post_id])	
+		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment])
 		redirect_to post_path(@post)
-	end
-
-	def show
-		@post = Post.find(params[:post_id])
 	end
 end
