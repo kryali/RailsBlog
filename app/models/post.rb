d9c71e6866ab1c46b97c5c0f57f136fc201843a2
Kiran Ryali <kiran.ryali@gmail.com>
2010-12-23 01:09:43 -0500

    added comments model, need to define controller

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+
+validates :name, :presence => true
+validates :content, :presence => true
+
+has_many :comments
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RailsBlog::Application.routes.draw do
+
+	resources :posts do
+	  resources :comments
+	end
   get "home/index"
   root :to => "home#index"
 
