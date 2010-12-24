35ceebac7a28c23a7b008846274b90bc7b536bc7
Kiran Ryali <kiran.ryali@gmail.com>
2010-12-24 02:06:15 -0500

    set up user functionality and displays

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(params[:user])
+		@user.save
+		redirect_to('/users/')
+	end
 end
